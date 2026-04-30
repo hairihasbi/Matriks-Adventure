fcda7e1a62b7b603e1c641b1652177a39b091d4c
hairihasbi <31791071+hairihasbi@users.noreply.github.com>
2026-05-01 07:54:04 +0800

    Add files via upload

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -7,6 +7,11 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
   readonly VITE_FIREBASE_APP_ID: string
+  readonly VITE_FIREBASE_MEASUREMENT_ID?: string
+  readonly VITE_GEMINI_API_KEY?: string
+  readonly VITE_GEMINI_API_KEY_1?: string
+  readonly VITE_GEMINI_API_KEY_2?: string
+  readonly VITE_GEMINI_API_KEY_3?: string
 }
 
 interface ImportMeta {
